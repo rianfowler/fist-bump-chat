fc8094dc135a6ad8f062a4645d279c96f36706a2
Rian Fowler <rianf@me.com>
2023-10-30 13:45:34 -0500

    fix: :bug: Fix background scrolling on feed page
    Content would overflow past the grey background on scrolling. Also the menu bar scroll down too far as the scrolling collapses the border and margin.

--- internal/handlers/middleware.go
+++ internal/handlers/middleware.go
@@ -82,7 +82,7 @@ func (mum *MockUserMiddleware) Middleware(c *fiber.Ctx) error {
 			return c.Status(500).SendString(err.Error())
 		}
 
-		for i := 0; i < 1000; i++ {
+		for i := 0; i < 1; i++ {
 			_, err = mum.repo.CreateMessage(c.Context(), datastore.Message{
 				Message:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 				UserId:   u.ID,
